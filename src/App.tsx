7459a8bdd9a17f0bc287fd4581ccef3098075fb9
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2021-05-04 21:33:19 +0800

    feat(react bootstrap): install and integrate base setup for bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.scss";
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleChange = (event: React.ChangeEvent<any>) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(event.target.files[0], "UTF-8");
+    fileReader.onload = (event: ProgressEvent<FileReader>) => {
+      console.log(event.target?.result);
+    };
+  }
+  render() {
+    return (
+      <Container>
+        <Form>
+          <Form.Group>
+            <Form.File id="exampleFormControlFile1" label="Select JSON File" onChange={this.handleChange} />
+          </Form.Group>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default App;
